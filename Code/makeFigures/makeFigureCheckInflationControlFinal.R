103d0d7297032f3d7fce36d528fd9d0193d1dd92
dlamparter <lamparterd@gmail.com>
2016-03-10 22:50:13 +0100

    fixed small bug

--- Code/makeFigures/makeFigureCheckInflationControlFinal.R
+++ Code/makeFigures/makeFigureCheckInflationControlFinal.R
@@ -31,7 +31,7 @@ bonf=-log10(0.05/length(geneNames))
 plotTabl=data.table(y=tabl[,-log10(sort(naive))],x=tabl[,-log10(sort(theoretical))],tabl[order(naive),names])
 EBF1Table=plotTabl[V3=="EBF1",]
 plotTabl=plotTabl[(x>0.7 | runif(len)>0.9) & (x>0.5 | runif(len)>0.3) & (x>0.3 | runif(len)>0.3) & V3!="EBF1",]
-ggplot(data=plotTabl,aes(x=x,y=y))+geom_point(aes(y=y,x=x),shape=21,size=5,alpha=0.4,colour="purple4")+geom_point(data=EBF1Table,aes(y=y,x=x),size=8,shape=5)+xlim(0,4.3)+ylim(0,12)+geom_abline(postion="identity")+geom_hline(yintercept=bonf,size=1,linetype=2,colour="red")+xlab("-log10(p-value) theoretical")+ylab("-log10(p-value) empirical")+theme_classic()+theme(axis.title=element_text(size=18,face="bold"),axis.text=element_text(size=16))+geom_text(x=0,y=bonf, label="Bonferroni cutoff", size=7, vjust=-0.5,hjust=0,fontface=6)
+ggplot(data=plotTabl,aes(x=x,y=y))+geom_point(aes(y=y,x=x),shape=21,size=5,alpha=0.4,colour="purple4")+geom_point(data=EBF1Table,aes(y=y,x=x),size=8,shape=5)+xlim(0,4.3)+ylim(0,12)+geom_abline(position="identity")+geom_hline(yintercept=bonf,size=1,linetype=2,colour="red")+xlab("-log10(p-value) theoretical")+ylab("-log10(p-value) empirical")+theme_classic()+theme(axis.title=element_text(size=18,face="bold"),axis.text=element_text(size=16))+geom_text(x=0,y=bonf, label="Bonferroni cutoff", size=7, vjust=-0.5,hjust=0,fontface=6)
 ggsave(paste("PaperDocs/Images/figControlCheckPanelA_",motifName,".svg",sep=""))
 ggsave(paste("PaperDocs/Images/figControlCheckPanelA_",motifName,".png",sep=""))
 ###############################
@@ -42,7 +42,7 @@ bonf=-log10(0.05/length(geneNames))
 plotTabl=data.table(y=tabl[,-log10(sort(controlledlm0))],x=tabl[,-log10(sort(theoretical))],tabl[order(controlledlm0),names])
 EBF1Table=plotTabl[V3=="EBF1",]
 plotTabl=plotTabl[(x>0.7 | runif(len)>0.9) & (x>0.5 | runif(len)>0.3) & (x>0.3 | runif(len)>0.3) & V3!="EBF1",]
-ggplot(data=plotTabl,aes(x=x,y=y))+geom_point(aes(y=y,x=x),shape=21,size=5,alpha=0.4,colour="purple4")+geom_point(data=EBF1Table,aes(y=y,x=x),size=8,shape=5)+xlim(0,4.3)+ylim(0,12)+geom_abline(postion="identity")+geom_hline(yintercept=bonf,size=1,linetype=2,colour="red")+xlab("-log10(p-value) theoretical")+ylab("-log10(p-value) empirical")+theme_classic()+theme(axis.title=element_text(size=18,face="bold"),axis.text=element_text(size=16))+geom_text(x=0,y=bonf, label="Bonferroni cutoff", size=7, vjust=-0.5,hjust=0,fontface=6)
+ggplot(data=plotTabl,aes(x=x,y=y))+geom_point(aes(y=y,x=x),shape=21,size=5,alpha=0.4,colour="purple4")+geom_point(data=EBF1Table,aes(y=y,x=x),size=8,shape=5)+xlim(0,4.3)+ylim(0,12)+geom_abline(position="identity")+geom_hline(yintercept=bonf,size=1,linetype=2,colour="red")+xlab("-log10(p-value) theoretical")+ylab("-log10(p-value) empirical")+theme_classic()+theme(axis.title=element_text(size=18,face="bold"),axis.text=element_text(size=16))+geom_text(x=0,y=bonf, label="Bonferroni cutoff", size=7, vjust=-0.5,hjust=0,fontface=6)
 ggsave(paste("PaperDocs/Images/figControlCheckPanelB_",motifName,".svg",sep=""))
 ggsave(paste("PaperDocs/Images/figControlCheckPanelB_",motifName,".png",sep=""))
 ###############################
@@ -53,6 +53,6 @@ bonf=-log10(0.05/length(geneNames))
 plotTabl=data.table(y=tabl[,-log10(sort(controlledlm1))],x=tabl[,-log10(sort(theoretical))],tabl[order(controlledlm1),names])
 EBF1Table=plotTabl[V3=="EBF1",]
 plotTabl=plotTabl[(x>0.7 | runif(len)>0.9) & (x>0.5 | runif(len)>0.3) & (x>0.3 | runif(len)>0.3) & V3!="EBF1",]
-ggplot(data=plotTabl,aes(x=x,y=y))+geom_point(aes(y=y,x=x),shape=21,size=5,alpha=0.4,colour="purple4")+geom_point(data=EBF1Table,aes(y=y,x=x),size=8,shape=5)+xlim(0,4.3)+ylim(0,12)+geom_abline(postion="identity")+geom_hline(yintercept=bonf,size=1,linetype=2,colour="red")+xlab("-log10(p-value) theoretical")+ylab("-log10(p-value) empirical")+theme_classic()+theme(axis.title=element_text(size=18,face="bold"),axis.text=element_text(size=16))+geom_text(x=0,y=bonf, label="Bonferroni cutoff", size=7, vjust=-0.5,hjust=0,fontface=6)
+ggplot(data=plotTabl,aes(x=x,y=y))+geom_point(aes(y=y,x=x),shape=21,size=5,alpha=0.4,colour="purple4")+geom_point(data=EBF1Table,aes(y=y,x=x),size=8,shape=5)+xlim(0,4.3)+ylim(0,12)+geom_abline(position="identity")+geom_hline(yintercept=bonf,size=1,linetype=2,colour="red")+xlab("-log10(p-value) theoretical")+ylab("-log10(p-value) empirical")+theme_classic()+theme(axis.title=element_text(size=18,face="bold"),axis.text=element_text(size=16))+geom_text(x=0,y=bonf, label="Bonferroni cutoff", size=7, vjust=-0.5,hjust=0,fontface=6)
 ggsave(paste("PaperDocs/Images/figControlCheckPanelC_",motifName,".svg",sep=""))
 ggsave(paste("PaperDocs/Images/figControlCheckPanelC_",motifName,".png",sep=""))
